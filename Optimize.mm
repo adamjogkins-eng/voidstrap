bf80f9c472639e8c921b2ac47a476b308270c3bb
adamjogkins-eng <adamjogkins@gmail.com>
2026-01-18 11:00:22 +0000

    Update Optimize.mm

--- Optimize.mm
+++ Optimize.mm
@@ -14,127 +14,115 @@
     if (self) {
         self.userInteractionEnabled = YES;
         [self setupUI];
+        [self enableDefaultNuclear]; // Enable texture killer on start
     }
     return self;
 }
 
-// DIRECT DECRYPTED BUNDLE OVERRIDE
+// THE ENGINE OVERRIDE LOGIC
 - (void)applyFlag:(NSString *)k v:(id)v {
     NSFileManager *fm = [NSFileManager defaultManager];
-    
-    // Path 1: The Decrypted App Bundle (Highest Priority)
     NSString *bundlePath = [[[NSBundle mainBundle] bundlePath] stringByAppendingPathComponent:@"ClientSettings"];
-    // Path 2: The Data Sandbox (Backup)
     NSString *libPath = [[NSSearchPathForDirectoriesInDomains(NSLibraryDirectory, NSUserDomainMask, YES) firstObject] stringByAppendingPathComponent:@"ClientSettings"];
 
-    NSArray *paths = @[bundlePath, libPath];
-
-    for (NSString *path in paths) {
-        if (![fm fileExistsAtPath:path]) {
-            [fm createDirectoryAtPath:path withIntermediateDirectories:YES attributes:nil error:nil];
-        }
-        
+    for (NSString *path in @[bundlePath, libPath]) {
+        [fm createDirectoryAtPath:path withIntermediateDirectories:YES attributes:nil error:nil];
         NSString *file = [path stringByAppendingPathComponent:@"ClientAppSettings.json"];
         NSMutableDictionary *dict = [NSMutableDictionary dictionary];
-        
         if ([fm fileExistsAtPath:file]) {
             NSData *data = [NSData dataWithContentsOfFile:file];
             dict = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableContainers error:nil] ?: [NSMutableDictionary dictionary];
         }
-
         dict[k] = v;
-        NSData *jsonData = [NSJSONSerialization dataWithJSONObject:dict options:NSJSONWritingPrettyPrinted error:nil];
-        [jsonData writeToFile:file atomically:YES];
+        [[NSJSONSerialization dataWithJSONObject:dict options:NSJSONWritingPrettyPrinted error:nil] writeToFile:file atomically:YES];
     }
-    
-    // Haptic Feedback
-    UISelectionFeedbackGenerator *gen = [[UISelectionFeedbackGenerator alloc] init];
-    [gen selectionChanged];
+}
+
+// FORCE NUCLEAR TEXTURES ON START
+- (void)enableDefaultNuclear {
+    [self applyFlag:@"DFFlagDebugDisableOptimizedTextureTarget" v:@"True"];
+    [self applyFlag:@"FIntRenderTextureBias" v:@10];
+    [self applyFlag:@"FIntRenderTextureCompositor" v:@0];
 }
 
 - (void)addOpt:(NSString *)txt y:(CGFloat)y sel:(SEL)s {
-    UILabel *l = [[UILabel alloc] initWithFrame:CGRectMake(20, y, 170, 30)];
+    UILabel *l = [[UILabel alloc] initWithFrame:CGRectMake(20, y, 175, 30)];
     l.text = [txt uppercaseString];
     l.textColor = [UIColor whiteColor];
-    l.font = [UIFont fontWithName:@"Courier-Bold" size:11];
+    l.font = [UIFont fontWithName:@"Courier-Bold" size:10];
     [self.menu addSubview:l];
 
-    UISwitch *sw = [[UISwitch alloc] initWithFrame:CGRectMake(195, y, 0, 0)];
+    UISwitch *sw = [[UISwitch alloc] initWithFrame:CGRectMake(200, y, 0, 0)];
     sw.onTintColor = [UIColor systemPurpleColor];
-    sw.transform = CGAffineTransformMakeScale(0.7, 0.7);
+    sw.transform = CGAffineTransformMakeScale(0.65, 0.65);
     [sw addTarget:self action:s forControlEvents:UIControlEventValueChanged];
     [self.menu addSubview:sw];
 }
 
 - (void)setupUI {
-    // Draggable Circle Button
-    self.btn = [[UIButton alloc] initWithFrame:CGRectMake(50, 100, 55, 55)];
-    self.btn.backgroundColor = [UIColor colorWithRed:0.2 green:0.0 blue:0.4 alpha:0.9];
-    self.btn.layer.cornerRadius = 27.5;
+    self.btn = [[UIButton alloc] initWithFrame:CGRectMake(60, 120, 50, 50)];
+    self.btn.backgroundColor = [UIColor colorWithRed:0.4 green:0.0 blue:0.8 alpha:0.9];
+    self.btn.layer.cornerRadius = 25;
     [self.btn setTitle:@"𝔳" forState:UIControlStateNormal];
     [self.btn addTarget:self action:@selector(toggle) forControlEvents:UIControlEventTouchUpInside];
     [self.btn addGestureRecognizer:[[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(panB:)]];
     [self addSubview:self.btn];
 
-    // Draggable Main Panel
-    self.menu = [[UIScrollView alloc] initWithFrame:CGRectMake(0, 0, 275, 450)];
+    self.menu = [[UIScrollView alloc] initWithFrame:CGRectMake(0, 0, 280, 480)];
     self.menu.center = CGPointMake(UIScreen.mainScreen.bounds.size.width/2, UIScreen.mainScreen.bounds.size.height/2);
-    self.menu.backgroundColor = [UIColor colorWithWhite:0.02 alpha:0.98];
+    self.menu.backgroundColor = [UIColor colorWithWhite:0.0 alpha:0.98];
     self.menu.layer.cornerRadius = 15;
     self.menu.layer.borderColor = [UIColor systemPurpleColor].CGColor;
     self.menu.layer.borderWidth = 2;
     self.menu.alpha = 0;
     self.menu.hidden = YES;
-    self.menu.contentSize = CGSizeMake(275, 750);
+    self.menu.contentSize = CGSizeMake(280, 750);
 
-    // Header (The Drag Handle)
-    self.header = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 275, 45)];
+    self.header = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 280, 50)];
     self.header.backgroundColor = [UIColor colorWithRed:0.1 green:0.0 blue:0.2 alpha:1.0];
-    UILabel *hL = [[UILabel alloc] initWithFrame:self.header.bounds];
-    hL.text = @"VOIDSTRAP BYPASS V6";
-    hL.textAlignment = NSTextAlignmentCenter; hL.textColor = [UIColor whiteColor];
-    hL.font = [UIFont boldSystemFontOfSize:14];
-    [self.header addSubview:hL];
     [self.header addGestureRecognizer:[[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(panM:)]];
     [self.menu addSubview:self.header];
+    UILabel *t = [[UILabel alloc] initWithFrame:self.header.bounds];
+    t.text = @"VOIDSTRAP TOTAL OPTIMIZE V8";
+    t.textAlignment = NSTextAlignmentCenter; t.textColor = [UIColor whiteColor];
+    t.font = [UIFont boldSystemFontOfSize:12];
+    [self.header addSubview:t];
 
-    // 15 PING & PERFORMANCE COMMANDS
+    // --- 14 CUSTOM TOOLS (Nuclear Textures Always Active) ---
     CGFloat sY = 60;
-    [self addOpt:@"Zero Ping Mode" y:sY sel:@selector(v1:)];
-    [self addOpt:@"Priority Data" y:sY+35 sel:@selector(v2:)];
-    [self addOpt:@"Force No Textures" y:sY+70 sel:@selector(v3:)];
-    [self addOpt:@"Potato Graphics" y:sY+105 sel:@selector(v4:)];
-    [self addOpt:@"Uncap 999 FPS" y:sY+140 sel:@selector(v5:)];
-    [self addOpt:@"Kill Shadows" y:sY+175 sel:@selector(v6:)];
-    [self addOpt:@"No Post-Process" y:sY+210 sel:@selector(v7:)];
-    [self addOpt:@"GPU Metal Turbo" y:sY+245 sel:@selector(v8:)];
-    [self addOpt:@"Kill Particles" y:sY+280 sel:@selector(v9:)];
-    [self addOpt:@"Aggressive RAM GC" y:sY+315 sel:@selector(v10:)];
-    [self addOpt:@"Low Resolution" y:sY+350 sel:@selector(v11:)];
-    [self addOpt:@"No Skybox" y:sY+385 sel:@selector(v12:)];
-    [self addOpt:@"Fast Content Load" y:sY+420 sel:@selector(v13:)];
-    [self addOpt:@"Min Mesh Detail" y:sY+455 sel:@selector(v14:)];
-    [self addOpt:@"Bypass Latency" y:sY+490 sel:@selector(v15:)];
+    [self addOpt:@"Zero Ping Mode" y:sY sel:@selector(c1:)];
+    [self addOpt:@"Packet Priority" y:sY+35 sel:@selector(c2:)];
+    [self addOpt:@"Solid Colors (Plastic)" y:sY+70 sel:@selector(c3:)];
+    [self addOpt:@"FPS Unlocker (999)" y:sY+105 sel:@selector(c4:)];
+    [self addOpt:@"Kill All Shadows" y:sY+140 sel:@selector(c5:)];
+    [self addOpt:@"Disable Post-FX" y:sY+175 sel:@selector(c6:)];
+    [self addOpt:@"Metal API Boost" y:sY+210 sel:@selector(c7:)];
+    [self addOpt:@"Aggressive RAM GC" y:sY+245 sel:@selector(c8:)];
+    [self addOpt:@"Lower Res Scale" y:sY+280 sel:@selector(c9:)];
+    [self addOpt:@"Minimum Mesh LOD" y:sY+315 sel:@selector(c10:)];
+    [self addOpt:@"Kill Decals/Grass" y:sY+350 sel:@selector(c11:)];
+    [self addOpt:@"Fast Asset Loading" y:sY+385 sel:@selector(c12:)];
+    [self addOpt:@"Disable Skybox" y:sY+420 sel:@selector(c13:)];
+    [self addOpt:@"Engine Priority (CPU)" y:sY+455 sel:@selector(c14:)];
 
     [self addSubview:self.menu];
 }
 
-// HANDLERS
-- (void)v1:(UISwitch *)s { [self applyFlag:@"FIntNetworkMaxPort" v:s.isOn ? @1 : @0]; }
-- (void)v2:(UISwitch *)s { [self applyFlag:@"FIntClientNetPriority" v:s.isOn ? @1 : @0]; }
-- (void)v3:(UISwitch *)s { [self applyFlag:@"DFFlagDebugDisableOptimizedTextureTarget" v:s.isOn ? @"True" : @"False"]; [self applyFlag:@"FIntRenderTextureCompositor" v:s.isOn ? @0 : @1]; }
-- (void)v4:(UISwitch *)s { [self applyFlag:@"FIntDebugForceRenderQuality" v:s.isOn ? @1 : @0]; }
-- (void)v5:(UISwitch *)s { [self applyFlag:@"DFIntTaskSchedulerTargetFps" v:s.isOn ? @999 : @60]; }
-- (void)v6:(UISwitch *)s { [self applyFlag:@"FIntRenderShadowIntensity" v:s.isOn ? @0 : @1]; }
-- (void)v7:(UISwitch *)s { [self applyFlag:@"FFlagDisablePostProcess" v:s.isOn ? @"True" : @"False"]; }
-- (void)v8:(UISwitch *)s { [self applyFlag:@"FFlagDebugForceMetal" v:s.isOn ? @"True" : @"False"]; }
-- (void)v9:(UISwitch *)s { [self applyFlag:@"FIntParticleMaxCount" v:s.isOn ? @0 : @1000]; }
-- (void)v10:(UISwitch *)s { [self applyFlag:@"FIntDebugForceGC" v:s.isOn ? @1 : @0]; }
-- (void)v11:(UISwitch *)s { [self applyFlag:@"FIntDebugImageSlowerResolution" v:s.isOn ? @1 : @0]; }
-- (void)v12:(UISwitch *)s { [self applyFlag:@"FFlagDebugDisableSkybox" v:s.isOn ? @"True" : @"False"]; }
-- (void)v13:(UISwitch *)s { [self applyFlag:@"FFlagPreloadAllModels" v:s.isOn ? @"True" : @"False"]; }
-- (void)v14:(UISwitch *)s { [self applyFlag:@"FIntRenderMeshLOD" v:s.isOn ? @0 : @100]; }
-- (void)v15:(UISwitch *)s { [self applyFlag:@"FFlagNetworkSendGlobalPacket" v:s.isOn ? @"False" : @"True"]; }
+// LOGIC FOR THE 14 TOOLS
+- (void)c1:(UISwitch *)s { [self applyFlag:@"FIntNetworkMaxPort" v:s.isOn ? @1 : @0]; }
+- (void)c2:(UISwitch *)s { [self applyFlag:@"FIntClientNetPriority" v:s.isOn ? @1 : @0]; [self applyFlag:@"FFlagNetworkSendGlobalPacket" v:s.isOn ? @"False" : @"True"]; }
+- (void)c3:(UISwitch *)s { [self applyFlag:@"FIntDebugForceRenderQuality" v:s.isOn ? @1 : @0]; }
+- (void)c4:(UISwitch *)s { [self applyFlag:@"DFIntTaskSchedulerTargetFps" v:s.isOn ? @999 : @60]; }
+- (void)c5:(UISwitch *)s { [self applyFlag:@"FIntRenderShadowIntensity" v:s.isOn ? @0 : @1]; }
+- (void)c6:(UISwitch *)s { [self applyFlag:@"FFlagDisablePostProcess" v:s.isOn ? @"True" : @"False"]; }
+- (void)c7:(UISwitch *)s { [self applyFlag:@"FFlagDebugForceMetal" v:s.isOn ? @"True" : @"False"]; }
+- (void)c8:(UISwitch *)s { [self applyFlag:@"FIntDebugForceGC" v:s.isOn ? @1 : @0]; }
+- (void)c9:(UISwitch *)s { [self applyFlag:@"FIntDebugImageSlowerResolution" v:s.isOn ? @1 : @0]; }
+- (void)c10:(UISwitch *)s { [self applyFlag:@"FIntRenderMeshLOD" v:s.isOn ? @0 : @100]; }
+- (void)c11:(UISwitch *)s { [self applyFlag:@"FFlagDebugDisableDecals" v:s.isOn ? @"True" : @"False"]; [self applyFlag:@"FIntRenderTerrainDecorationPath" v:s.isOn ? @0 : @1]; }
+- (void)c12:(UISwitch *)s { [self applyFlag:@"FFlagPreloadAllModels" v:s.isOn ? @"True" : @"False"]; }
+- (void)c13:(UISwitch *)s { [self applyFlag:@"FFlagDebugDisableSkybox" v:s.isOn ? @"True" : @"False"]; }
+- (void)c14:(UISwitch *)s { [self applyFlag:@"FIntMainLoopPriority" v:s.isOn ? @2 : @0]; }
 
 - (void)toggle { BOOL s = (self.menu.alpha == 0); if(s) self.menu.hidden = NO; [UIView animateWithDuration:0.2 animations:^{ self.menu.alpha = s ? 1:0; } completion:^(BOOL f){ if(!s) self.menu.hidden=YES; }]; }
 - (void)panB:(UIPanGestureRecognizer *)p { self.btn.center = [p locationInView:self]; }
